422fe6777e23312c5bde03a8acbdd83da921e0f0
Esteban Ocampo <estebanocampo114@gmail.com>
2022-11-08 16:22:11 -0500

    Commit inicial

---
+++ OPMApi/src/main/java/com/example/OPMApi/OpmApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.OPMApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpmApiApplication.class, args);
+	}
+
+}
---
+++ OPMApi/src/test/java/com/example/OPMApi/OpmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OPMApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
